c55644392d7e1f363bb1c076a80f9965a7435482
coderdojokells <43277582+coderdojokells@users.noreply.github.com>
2018-09-15 15:32:33 +0100

    Add files via upload

---
+++ projects/JavaScript/Intermediate JavaScript/Intermediate JavaScript - Starter Project/to-do.js
@@ -0,0 +1,36 @@
+// This function creates a new to-do list item
+function newToDoItem(itemText, completed) {
+  alert("New item created: "+itemText+"  Completed state: "+completed)
+}
+
+// This function adds an item to the to-do list
+function addToDoItem() {
+  alert("Item added!")
+}
+
+// This function cleans up all the completed to-do items
+function clearCompletedToDoItems() {
+  alert("Clearing to-do items!")
+}
+
+// This function resets the to-do list to blank
+function emptyList() {
+  alert("Emptying list!")
+}
+
+// This function loads the to-do list when the page loads
+function loadList() {
+  alert("Loaded to-do list")
+}
+
+// This funciton gets the HTML code to display a to-do item
+function getToDoItemHTML(toDoItem) {
+  alert("Getting HTML of "+toDoItem)
+}
+
+// Things to do when the page loads
+function runWhenPageLoads(){
+  // Your code here!
+}
+
+runWhenPageLoads()
